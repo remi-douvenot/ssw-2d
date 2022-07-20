c2df868a1dd2064d913115f0ffb88f30495b014b
vdarchy <victor.darchy-ext@enac.fr>
2022-07-20 14:41:14 +0200

    Update log_variance.py

--- post_processing/src/plots/log_variance.py
+++ post_processing/src/plots/log_variance.py
@@ -40,134 +40,36 @@ e_field_standard_1G_Los100 = np.load('./outputs/E_field_standard_1G_40km_Los100.
 
 
 
-
+#Computation of the numerical log-amplitude variance
 def log_variance(config, E_turbulent,E_reference):
-    # -- Constants-- #
-    Cn2 = config.Cn2  # exponent
-    c0 = cst.c  # m/s
-    Los = config.L0  # Large scale
-
+    # --- set parameters --#
     n_x = config.N_x
     sigma2 = np.zeros(n_x)
     n_z = config.N_z
     n_apo_z = np.int(config.apo_z * n_z)
+    # --- compute vertical log-variance at each range step --#
     for ii_x in range(0, n_x):
         ln_amplitude = np.zeros(n_z)
         print(ii_x)
-        for ii_z in range(n_apo_z, n_z - n_apo_z):  # Prise en compte du champ seulement dans la fenêtre d'apodisation
+        for ii_z in range(n_apo_z, n_z - n_apo_z): #Field is true only in apo window
             ln_amplitude[ii_z]=np.log(np.abs(E_turbulent[ii_x][ii_z]) / np.abs(E_reference[ii_x][ii_z]))
         sigma2[ii_x]= np.var(ln_amplitude)
     # np.save('sigma2')
     return sigma2
 
-# Function that computes the numerical log-amplitude variance
-"""
-def log_variance_1G_Los10(config, E_turbulent):
-    # -- Constants-- #
-    Cn2 = config.Cn2  # exponent
-    c0 = cst.c  # m/s
-    Los = config.L0  # Large scale
-
-
-
-    # sigma2 = np.load('sigma2') #Comme ça à chaque appelle de fonction on appelle le sigma2 moyenné issue des simus précédentes
-    n_x = config.N_x
-    sigma2 = [0]*n_x
-    n_z = config.N_z
-    n_apo_z = np.int(config.apo_z * n_z)
-    for ii_x in range(0, n_x):
-        ln_amplitude = [0]*n_z
-        print(ii_x)
-        for ii_z in range(n_apo_z, n_z - n_apo_z):  # Prise en compte du champ seulement dans la fenêtre d'apodisation
-            ln_amplitude[ii_z]=np.log(np.abs(E_turbulent[ii_x][ii_z]) / np.abs(e_field_standard_1G_Los10[ii_x][ii_z]))
-        sigma2[ii_x]= np.var(ln_amplitude)
-    # np.save('sigma2')
-    return sigma2
-"""
-
-
-def log_variance_1G_Los100(config, E_turbulent):
-    # -- Constants-- #
-    Cn2 = config.Cn2  # exponent
-    c0 = cst.c  # m/s
-    Los = config.L0  # Large scale
-
-
-
-    # sigma2 = np.load('sigma2') #Comme ça à chaque appelle de fonction on appelle le sigma2 moyenné issue des simus précédentes
-    n_x = config.N_x
-    sigma2 = np.zeros(n_x)
-    n_z = config.N_z
-    n_apo_z = np.int(config.apo_z * n_z)
-    for ii_x in range(0, n_x):
-        ln_amplitude = np.zeros(n_z)
-        print(ii_x)
-        for ii_z in range(n_apo_z, n_z - n_apo_z):  # Prise en compte du champ seulement dans la fenêtre d'apodisation
-            ln_amplitude[ii_z]=np.log(np.abs(E_turbulent[ii_x][ii_z]) / np.abs(e_field_standard_1G_Los100[ii_x][ii_z]))
-        sigma2[ii_x]= np.var(ln_amplitude)
-    # np.save('sigma2')
-    return sigma2
-
-"""
-def log_variance_10G_Los10(config, E_turbulent):
-    # -- Constants-- #
-    Cn2 = config.Cn2  # exponent
-    c0 = cst.c  # m/s
-    Los = config.L0  # Large scale
-
-
-
-    # sigma2 = np.load('sigma2') #Comme ça à chaque appelle de fonction on appelle le sigma2 moyenné issue des simus précédentes
-    n_x = config.N_x
-    sigma2 = [0]*n_x
-    n_z = config.N_z
-    n_apo_z = np.int(config.apo_z * n_z)
-    for ii_x in range(0, n_x):
-        ln_amplitude = [0]*n_z
-        print(ii_x)
-        for ii_z in range(n_apo_z, n_z - n_apo_z):  # Prise en compte du champ seulement dans la fenêtre d'apodisation
-            ln_amplitude[ii_z]=np.log(np.abs(E_turbulent[ii_x][ii_z]) / np.abs(e_field_standard_10G_Los10[ii_x][ii_z]))
-        sigma2[ii_x]= np.var(ln_amplitude)
-    # np.save('sigma2')
-    return sigma2
-"""
-
-"""
-def log_variance_10G_Los100(config, E_turbulent):
-    # -- Constants-- #
-    Cn2 = config.Cn2  # exponent
-    c0 = cst.c  # m/s
-    Los = config.L0  # Large scale
-
-
-
-    # sigma2 = np.load('sigma2') #Comme ça à chaque appelle de fonction on appelle le sigma2 moyenné issue des simus précédentes
-    n_x = config.N_x
-    sigma2 = [0]*n_x
-    n_z = config.N_z
-    n_apo_z = np.int(config.apo_z * n_z)
-    for ii_x in range(0, n_x):
-        ln_amplitude = [0]*n_z
-        print(ii_x)
-        for ii_z in range(n_apo_z, n_z - n_apo_z):  # Prise en compte du champ seulement dans la fenêtre d'apodisation
-            ln_amplitude[ii_z]= np.log(np.abs(E_turbulent[ii_x][ii_z]) / np.abs(e_field_standard_10G_Los100[ii_x][ii_z]))
-        sigma2[ii_x]=  np.var(ln_amplitude)
-    # np.save('sigma2')
-    return sigma2
-"""
-# test = log_amplitude_variance_2D(e_field_Homo_10G_Los10_test, e_field_Cn12_Los100_40G_test)
 
 
 #Computation of the analytic log-amplitude variance 
 def logvar_analytic(config):
+    #--- set parameters --#
     Kos = 2*np.pi/config.L0
     k_0 = 2*np.pi*config.freq/3e8
     n_x = config.N_x
-    n_z = config.N_z
     R = n_x*config.x_step #total range
     X = np.linspace(100,R,n_x)
-    sigma2 = [0]*n_x
+    sigma2 = np.zeros(n_x)
     Np2dB = 8.68
+    # --- compute vertical log-variance at each range step --#
     for ii_x in range(n_x) :
         x = X[ii_x]
         #f = lambda K_z,u : Np2dB**2*2*np.pi*k_0**2*x*0.055*Cn2*(K_z**2+ Kos**2)**(-4/3) *0.5 * (1-np.cos(K_z**2*x*u*(1-u)/k_0)) #2D spherical waves
@@ -178,5 +80,4 @@ def logvar_analytic(config):
         print(x,F[1])
     return sigma2
 
-# 1G
-# sigma2_analytic = calc_variance_log(40000,10,100,f[0])
+
